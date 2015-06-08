d6a18ce061df62c70afbff2f6c4e77211c33fb81
shivraj chari <shivraj.goa@gmail.com>
2015-06-08 14:06:49 +0530

    Seperated Library,config,and demo files

---
+++ demo/application/config/facebook_config.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//facebook configuration file 
+
+$config['facebook_app_id']='';
+$config['facebook_app_secret']='';
+$config['facebook_app_redirect_url']='';
